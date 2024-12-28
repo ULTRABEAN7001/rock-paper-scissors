15981ce89a1d61ddff6160bb51f1e5da70aa29a2
ULTRABEAN7001 <109013326+ULTRABEAN7001@users.noreply.github.com>
2024-12-29 00:59:14 +0800

    Prevents excessive number of rounds if you already won or lost

--- script.js
+++ script.js
@@ -1,76 +1,48 @@
-let compChoices = ["rock", "paper", "scissors"]
+let choices = ["rock", "paper", "scissors"]
+let humanScore = 1;
+let computerScore = 1;
 
-// This decides the cpu's choices
-function getComputerChoice(choices){
-    let choice = Math.floor(Math.random() * choices.length);
-    return choices[choice];
+function getComputerChoice(){
+    let n;
+    n = choices[Math.floor(Math.random()* choices.length)];
+    return n
 }
 
-let playerPoints = 0;
-let computerPoints = 0;
+function getHumanChoice(){
+    let d = prompt("Pick sumthn");
+    return d
+}
 
-let computerSelection = getComputerChoice(compChoices);
-let playerSelection;
-
-
-// Player input is gathered and evaluated
-function startRound(playerSelection, computerSelection){
-        computerSelection = getComputerChoice(compChoices);
-        playerSelection = prompt("pick something").toLowerCase();
-
-    if (playerSelection == computerSelection){
-            console.log("Tied")
-            return startRound(playerSelection, computerSelection);
-    }   else if (playerSelection == "rock" && computerSelection == "paper"){
-            computerPoints = computerPoints+1;
-            return "You Lose! Paper beats Rock";
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.trim().toLowerCase();
+    computerChoice = computerChoice.trim().toLowerCase();
+    
+    if (humanScore == 3 || computerScore == 3){
+        return null
     }
-        else if (playerSelection == "paper" && computerSelection == "scissors"){
-            computerPoints = computerPoints+1;
-            return "You Lose! Scissors beats Paper";
-    }
-
-        else if (playerSelection == "scissors" && computerSelection == "rock"){
-            computerPoints = computerPoints+1;
-            return "You Lose! Rock beats Scissors";
-    }   
-
-        else if (playerSelection == "paper" && computerSelection == "rock"){
-            playerPoints = playerPoints+1;
-            return "You Win! Paper beats Rock";
-    }
-
-        else if (playerSelection == "scissors" && computerSelection == "paper"){
-            playerPoints = playerPoints+1;
-            return "You Win! Scissors beats Paper";
-    }
-
-        else if (playerSelection == "rock" && computerSelection == "scissors"){
-            playerPoints = playerPoints+1;
-            return "You Win! Rock beats Scissors";
-    }
-        else{
-            return startRound(playerSelection, computerSelection);
+    else if (humanScore < 3 && computerScore < 3){
+        if (humanChoice == computerChoice){
+            console.log("It's a tie");
         }
     
+        else if ((humanChoice == "rock" && computerChoice == "paper")||(humanChoice == "paper" && computerChoice == "scissors")||(humanChoice == "scissors" && computerChoice == "rock")){
+            console.log("You Lose");
+            computerScore += 1;
+        }
+    
+        else if ((humanChoice == "paper" && computerChoice == "rock")||(humanChoice == "scissors" && computerChoice == "paper")||(humanChoice == "rock" && computerChoice == "scissors")){
+            console.log("You Win");
+            humanScore += 1;
+        }
+        playRound(getHumanChoice(), getComputerChoice());
+    }    
 }
 
+playRound(getHumanChoice(), getComputerChoice())
 
-//Round is logged to the console
-function game(){
-    console.log(startRound(playerSelection, computerSelection));
-    console.log(startRound(playerSelection, computerSelection));
-    console.log(startRound(playerSelection, computerSelection));
-    console.log(startRound(playerSelection, computerSelection));
-    console.log(startRound(playerSelection, computerSelection));
-
-    if (playerPoints >= 3){
-        console.log("You Win")
-    }
-    else if (computerPoints >=3){
-        console.log("You Lose")
-    }
+if (humanScore == 3){
+    console.log("yay")
 }
-
-game();
-
+else if (computerScore == 3){
+    console.log("nay")
+}
